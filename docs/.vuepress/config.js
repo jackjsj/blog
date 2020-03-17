8fcf9d3fd03882cd27284f52c6161ca46dfb57a4
jackjsj <jackjsj@foxmail.com>
2020-03-17 17:15:45 +0800

    init blog framework

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "Null的博客",
+  description: "个人的收集整理箱",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/home" },
+      { text: "Github", link: "https://github.com/jackjsj" }
+    ]
+  }
+};
